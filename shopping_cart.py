cb60399b381327b3160c20eab9e4114fc59e1544
Alyssa Aldridge <ana81@georgetown.edu>
2021-02-17 13:28:03 -0500

    Total Price Sum

--- shopping_cart.py
+++ shopping_cart.py
@@ -44,25 +44,25 @@ def to_usd(my_price):
 #
 # Information Input
 #
+
+total_price = 0
+
 while True:
-        selected_item = input("Please input Product ID (1-20 is valid):")
-
-        if selected_item == "DONE":
-            break
-        else:
-            matching_products = [item for item in products if str(item["id"]) == str(selected_item)]
-            matching_product = matching_products[0]
-            print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-
-
-# print(matching_products)
-# print(type(matching_products))
-
+    selected_item = input("Please input Product ID (1-20 is valid):")
+    if selected_item == "DONE":
+        break
+    else:
+        matching_products = [item for item in products if str(item["id"]) == str(selected_item)]
+        matching_product = matching_products[0]
+        total_price = total_price+ matching_product["price"]
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 #
 # Information Output
 #
 
+
+print("TOTAL PRICE:" + str(total_price))
 
 
 
